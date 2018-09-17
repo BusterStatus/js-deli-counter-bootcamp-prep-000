8039b231067e6ddf3dea34b7f78b741b7249c79d
matthew.serre <matthew.serre@gmail.com>
2018-09-17 00:33:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function currentLine (currentLineOfPeople) {
     message = "The line is currently: ";
     var person = (i+1) + ". " + currentLineOfPeople[i];
     message = message + person;
+    } else {
+      message = "The line is currently empty."
     }
   }
   return message;

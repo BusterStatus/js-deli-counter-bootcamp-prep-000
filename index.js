27709d42f77611124f63bccd906301c55b0fab66
matthew.serre <matthew.serre@gmail.com>
2018-09-17 00:21:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber (currentLineOfPeople, newPersonName) {
   currentLineOfPeople.push(newPersonName);
-  var welcomeMessage = "Welcome, " + newPersonName + ". You are number " + currentLineOfPeople.length + "in line.";
+  var welcomeMessage = "Welcome, " + newPersonName + ". You are number " + currentLineOfPeople.length + " in line.";
   return welcomeMessage;
 }
 
